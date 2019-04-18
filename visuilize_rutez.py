680d2dae4b434862c266a01fc95c00a2acd18302
Arthanadftz <43166097+Arthanadftz@users.noreply.github.com>
2019-04-18 14:07:46 +0300

    Add files via upload
    "Updated func@

--- visuilize_rutez.py
+++ visuilize_rutez.py
@@ -7,14 +7,8 @@ rutez.load()
 
 sinsets_keys = list(sorted(rutez.sinsets.keys()))
 
-#print(sinsets_keys)
-root = "ПОСТОЯННАЯ СУЩНОСТЬ"
-root_container = rutez.sinsets[root]
-root_sins = root_container['words']
-root_rels = root_container['relations']
-
-
-#print(root_rels)
+roots = [sinset[0] for sinset in rutez.top_sinsets()]
+print(roots)
 """
 root = "ПОСТОЯННАЯ СУЩНОСТЬ"
 root_container = rutez.sinsets[root]
@@ -38,9 +32,9 @@ for rel in root_rels:
 with open('vis_dict.json', 'w') as f:
     json.dump(root_dict, f, indent=4, ensure_ascii=False)
 """
-root = "ПОСТОЯННАЯ СУЩНОСТЬ"
-root_container = rutez.sinsets[root]
-root_sins = root_container['words']
+#root = "ПОСТОЯННАЯ СУЩНОСТЬ"
+#root_container = rutez.sinsets[root]
+#root_sins = root_container['words']
 
 def dig_deep(root_key):
     root_rels = rutez.sinsets[root_key]['relations']
@@ -48,12 +42,11 @@ def dig_deep(root_key):
     #, "Synonyms": rutez.sinsets[rel[1]]['words']
 
     d = {rel[1]: dig_deep(rel[1]) for rel in root_rels if rel[0] == "НИЖЕ"}
-    d["Synonyms"] = root_sins
-    #print(d)
-    return d
 
-    #for key in d:
-    #    d[key] = dig_deep(key)
+    if root_sins:
+        d["Synonyms"] = root_sins
+
+    return d
 
     """
     for rel in root_rels:
@@ -63,10 +56,84 @@ def dig_deep(root_key):
             #root_dict["Synonyms"] = root_sins
     """
 
-
+#root_dict = {"Root": [{root: dig_deep(root)} for root in roots]}
+#root_dict = {"Root": {root: dig_deep(root)} for root in roots}
+root_dict = {root: dig_deep(root) for root in roots}
+root_root = {"Root": root_dict}
+#root_lst = [{root: dig_deep(root)} for root in roots]
 #dig_deep(root)
-root_dict = {root: dig_deep(root), "Synonyms": root_sins}
+#root_dict = {root: dig_deep(root), "Synonyms": root_sins}
 #print(root_dict)
 
 with open('vis_dict_test.json', 'w') as f:
-    json.dump(root_dict, f, indent=4, ensure_ascii=False)
+    #json.dump(root_dict, f, indent=4, ensure_ascii=False)
+    json.dump(root_root, f, indent=4, ensure_ascii=False)
+
+
+def dig_deep(root_key):
+    root_rels = rutez.sinsets[root_key]['relations']
+    #root_sins = rutez.sinsets[root_key]['words']
+    d = {"name": root_key, "children": [dig_deep(rel[1]) for rel in root_rels if rel[0] == "НИЖЕ"]}
+    if not d["children"]:
+        del d["children"]
+    #for rel in root_rels:
+    #    if rel[0] == "НИЖЕ":
+    #        d["name"] = rel[1]
+    #        d["children"].append(dig_deep(rel[1]))
+
+    return d
+            #d = {"name": rel[1], "children": [dig_deep(rel[1])]}
+            #return d
+
+    #if root_sins:
+    #    d["Synonyms"] = root_sins
+
+    #return d
+root_graph = {"name": "Rutez", "children": [dig_deep(root) for root in roots]}
+
+
+
+#root_graph_ = {dig_deep(root) for root in roots}
+
+#root_graph = {"name": "Root", "children": root_graph_}
+
+with open('rutez_graph.json', 'w') as f:
+    json.dump(root_graph, f, indent=4, ensure_ascii=False)
+
+"""
+{
+ "name": "Root",
+ "children": [
+  {
+   "name": "analytics",
+   "children": [
+    {
+     "name": "cluster",
+     "children": [
+      {"name": "AgglomerativeCluster", "size": 3938},
+      {"name": "CommunityStructure", "size": 3812},
+      {"name": "HierarchicalCluster", "size": 6714},
+      {"name": "MergeEdge", "size": 743}
+     ]
+    },
+    {
+     "name": "graph",
+     "children": [
+      {"name": "BetweennessCentrality", "size": 3534},
+      {"name": "LinkDistance", "size": 5731},
+      {"name": "MaxFlowMinCut", "size": 7840},
+      {"name": "ShortestPaths", "size": 5914},
+      {"name": "SpanningTree", "size": 3416}
+     ]
+    },
+    {
+     "name": "optimization",
+     "children": [
+      {"name": "AspectRatioBanker", "size": 7074}
+     ]
+    }
+   ]
+  }
+ ]
+}
+"""
